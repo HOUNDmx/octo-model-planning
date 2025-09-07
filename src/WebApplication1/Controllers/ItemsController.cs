a4b37089cb507617f559fd086c9ee0b5dc5a90b6
HOUNDmx <crysramyrez@live.com>
2025-09-07 14:00:09 +0200

    starting point, mvc crud

---
+++ src/WebApplication1/Controllers/ItemsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelProductionCase.Data;
+using ModelProductionCase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ModelProductionCase.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ModelProductionContext _context;
+        public ItemsController(ModelProductionContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _context.Items.ToListAsync();
+            return View(items);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Status")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Status")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/WebApplication1/Data/ModelProductionContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ModelProductionCase.Data
+{
+    public class ModelProductionContext : DbContext
+    {
+        public ModelProductionContext(DbContextOptions<ModelProductionContext> options)
+            : base(options) { }
+        public DbSet<ModelProductionCase.Models.Item> Items { get; set; }
+    }
+}
